b9e518cfa35874502435fbe2bc6d21f88715e402
raceredge <aman.srivastav@cadfem.ai>
2026-06-18 14:57:19 +0530

    OT cleanup

--- src/features/OT/components/Hydro.jsx
+++
@@ -1,8 +0,0 @@
-export default function Hydro() {
-  return (
-    <div>
-      <h3>Hydro Rehabilitation</h3>
-      <p>Neuro-specific assessments and interventions go here.</p>
-    </div>
-  );
-}
